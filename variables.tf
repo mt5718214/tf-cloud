bcce3d070f3a7435203f203c3561fe9d2e54e77d
kemplin <mt5718214@gmail.com>
2024-07-05 09:47:10 +0800

    init first tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  default = "gosky-dev"
+}
+
+# variable "credentials_file" {}
+
+variable "region" {
+  default = "asia-east1"
+}
+
+variable "zone" {
+  default = "asia-east1-b"
+}
